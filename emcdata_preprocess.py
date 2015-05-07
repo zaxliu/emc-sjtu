7686aef7bfefc4308c4e98dcdf0af56f7935f4fa
desert-foxx <panhm13@163.com>
2015-05-07 18:16:22 +0800

    data_preprocess

--- emcdata_preprocess.py
+++ emcdata_preprocess.py
@@ -2,34 +2,33 @@
 # -*-coding:utf-8-*-
 #coding=utf-8
 """
-1.运行split_by_user.py，在user目录下生成按用户区分的文件
-
+本函数包含以下功能：
+1.对原始数据进行清洗，消除里面的所有NULL字符
+2.运行split_by_user.py中对应的分割函数，在user目录下生成按用户区分的文件
+3.运行emcdata_split.py中的生成Dict的函数，函数输入为需要分割的方式，根据分割方式在/EMCdata/Dictionary/目录下存储set和dictionary，输出为pkl文件对应的路径
 """
-
 import time
 from delete_space import deleteSpace
 from split_by_user import splitByUser
 # from gen_iid_geohash_category import genIid
 # from gen_uid_iid import genUidIid
-from emcdata_split import splitByUserandWeb
+from emcdata_split import generateDictBy
 
-# pdb.set_trace()
-if __name__ == "__main__":
+# value_style could be: Duration,CommunicationTotalByte,HTTPRequestNum,VisitingNum
+def data_preprocess(value_style):
     print "====================================="
     t0 = time.time()
-    # deleteSpace()
+    deleteSpace()
     t1 = time.time()
     print "It takes %f s to delete all spaces and generate 'EMC/EMCdata/*.csv'" %(t1-t0)
     splitByUser()
     t2 = time.time()
     print "It takes %f s to split by user,generate '/user/*.csv'" %(t2-t1)
-    splitByUserandWeb()
+    path = generateDictBy(value_style)
     t3 = time.time()
     print "It takes %f s to split by user and ISP,generate dictionary" %(t3-t2)
-#    genIid()
-#    t3 = time.time()
-#    print "It takes %f s to make dictionary{iid:[geohash,category]},generate 'data/dictionary/item.pkl'" %(t3-t2)
-#    genUidIid()
-#    t4 = time.time()
-#    print "It takes %f s to make dictionary{(uid,iid):[[b1,b2,b3,b4],[g1,g2..],[c1,c2..],[h1,h2..]]},generate 'data/dictionary/date/*.pkl'" %(t4-t3)
     print "====================================="
+    return path
+
+
+print data_preprocess('CommunicationTotalByte')
--- emcdata_split.py
+++ emcdata_split.py
@@ -1,12 +1,14 @@
 #coding=utf-8
 import csv
+import os
+import cPickle
 
 # 从原始数据中提取出userID和user访问的web，分别用两个set存储userID和userWeb的信息。然后生成一个Dictionary，其中的key是由userID和userWeb组合成的一个tuple，value是该用户访问该网站的总时长。
 final_dictionary = {}
 userid_set = set([])
 ISP_set = set([])
 
-def addTuple(user_id,isp,byte):
+def addTupleandByte(user_id,isp,byte):
     userid_set.add(user_id)
     ISP_set.add(isp)
     key_tuple = (user_id,isp)
@@ -16,14 +18,25 @@ def addTuple(user_id,isp,byte):
         final_dictionary[key_tuple] = final_dictionary[key_tuple] + byte
 
 
-def splitByUserandWeb():
-    f = open("../EMCdata/net_traffic_nospace.csv")
+def generateDictBy(para):
+    f = open("../EMCdata/net_traffic_sample.csv")
+    os.mkdir("../EMCdata/Dictionary/")
+    f_user = open("../EMCdata/Dictionary/userID_set.pkl", 'ab')
+    f_ISP = open("../EMCdata/Dictionary/ISP_set.pkl", 'ab')
+    f_dict = open("../EMCdata/Dictionary/"+para+".pkl", 'ab')
     rows = csv.reader(f)
-    for row in rows:
-        user_id = row[0]
-        ISP_total = row[4].split(";")
-        Com_byte = row[7].split(";")
-        for index,isp in enumerate(ISP_total):
-            addTuple(user_id,isp,Com_byte[index])
-    print final_dictionary
-
+    if para == 'CommunicationTotalByte':
+        for row in rows:
+            user_id = row[0]
+            ISP_total = row[4].split(";")
+            Com_byte = row[7].split(";")
+            for index,isp in enumerate(ISP_total):
+                addTupleandByte(user_id, isp, Com_byte[index])
+    cPickle.dump(final_dictionary,f_dict, -1)
+    f_dict.close()
+    cPickle.dump(userid_set,f_user, -1)
+    f_user.close()
+    cPickle.dump(ISP_set,f_ISP, -1)
+    f_ISP.close()
+    path = {'userID': '../EMCdata/Dictionary/ISP_set.pkl','visiting_ISP': '../EMCdata/Dictionary/ISP_set.pkl',para: '../EMCdata/Dictionary/'+para+'.pkl'}
+    return path
